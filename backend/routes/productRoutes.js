2d940ad5f676199faa66e9d97c6231a560a585ff
Dharmavarapu-satyasuryateja <suryateja.d788@gmail.com>
2026-04-17 16:12:34 +0530

    iamdeveloper

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express= require("express")
 const router=express.Router() 
-const {protect,authorize}=require("../middleware/authMiddleware")
+const {protect,authorize}=require("../middleware/authmiddleware")
 const Product=require("../models/Product")
 const productRoutes=express.Router()
 
